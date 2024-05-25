6d1ffa3c589ae632ee2a0917ad47c3c509121a73
raiyan <ray38250968@gmail.com>
2024-05-25 20:20:41 +0530

    chores

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,6 +1,6 @@
 async function fetchContentData() {
   try {
-    const response = await fetch("../data.json");
+    const response = await fetch(".././data.json");
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
